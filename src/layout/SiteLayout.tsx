0c82f1a4048d4060d9fc2301856b3d7410d5d430
Hinata0607 <atahin0607@gmail.com>
2024-09-22 02:15:39 +0900

    add: site header

--- src/layout/SiteLayout.tsx
+++ src/layout/SiteLayout.tsx
@@ -1,5 +1,19 @@
+'use client';
 import { SiteLayoutProps } from '@/interfaces';
+import styled from 'styled-components';
+import { Header } from './section';
 
 export const SiteLayout = ({ children }: SiteLayoutProps) => {
-	return <div>{children}</div>;
+	return (
+		<SLayout>
+			<Header />
+			{children}
+		</SLayout>
+	);
 };
+
+const SLayout = styled.section`
+	width: 100%;
+	height: 100vh;
+	background-color: #ada;
+`;
