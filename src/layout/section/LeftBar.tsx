83e56c34b9d3577cba8bf6ccb7d468c130bb88fa
Hinata0607 <atahin0607@gmail.com>
2024-09-22 02:26:44 +0900

    add: site leftbar

---
+++ src/layout/section/LeftBar.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const LeftBar = () => {
+	return <SLeftBar></SLeftBar>;
+};
+
+const SLeftBar = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	alignitems: center;
+	width: 300px;
+	height: 100%;
+	background-color: #dad;
+`;
