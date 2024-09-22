9eed593245cce233daea15221c4a97d9c282880c
Hinata0607 <atahin0607@gmail.com>
2024-09-22 17:34:54 +0900

    add: leftbar responsive

--- src/interfaces/layout/SiteLayoutProps.d.ts
+++ src/interfaces/layout/SiteLayoutProps.d.ts
@@ -3,3 +3,10 @@ import { ReactNode } from 'react';
 export interface SiteLayoutProps {
 	children: ReactNode;
 }
+
+export interface SMainProps {
+	$isSm: boolean;
+}
+export interface SChildrenProps {
+	$isSm: boolean;
+}
