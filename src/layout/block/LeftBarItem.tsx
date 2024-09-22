7468108aad6595fb23279832e1a57501fab46534
Hinata0607 <atahin0607@gmail.com>
2024-09-22 15:50:25 +0900

    add: prefecture leftbar list display

---
+++ src/layout/block/LeftBarItem.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useTheme } from '@/hooks';
+import { LeftBarItemProps } from '@/interfaces';
+import styled from 'styled-components';
+
+export const LeftBarItem = ({ prefName }: LeftBarItemProps) => {
+	const { darkTheme } = useTheme();
+
+	return <SLeftBarItem theme={darkTheme}>{prefName}</SLeftBarItem>;
+};
+
+const SLeftBarItem = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 70px;
+	flex-shrink: 0;
+	border-radius: 0 10px 10px 0;
+	cursor: pointer;
+	color: ${(props) => props.theme.text.main};
+
+	&:hover {
+		background-color: ${(props) => props.theme.layout.leftBar.item.hoverBg};
+	}
+`;
