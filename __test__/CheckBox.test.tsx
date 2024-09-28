7d46515bd0b04ced3a1a41d859e94f5fdd80bf19
Hinata0607 <atahin0607@gmail.com>
2024-09-28 09:59:18 +0900

    test: prefecture checkbox

---
+++ __test__/CheckBox.test.tsx
@@ -0,0 +1,111 @@
+import React, { act } from 'react';
+import '@testing-library/jest-dom';
+import { mockPrefectures, mockSelectedPrefectures } from './mock';
+import { ThemeProvider } from 'styled-components';
+import { ContextProvider } from './../src/provider';
+import { darkTheme } from './../src/theme';
+import { usePrefecture, useTheme } from './../src/hooks';
+import {
+	fireEvent,
+	render,
+	renderHook,
+	screen,
+	waitFor,
+} from '@testing-library/react';
+import { PrefectureSelectSm } from '../src/components/block';
+
+// usePrefectureをモック
+jest.mock('./../src/hooks', () => ({
+	usePrefecture: jest.fn(),
+	useTheme: jest.fn(),
+}));
+
+// テスト用コンポーネント(styeld-componentsを使用しているため、ThemeProviderを配置しています)
+const TestWrapper = () => {
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<ContextProvider>
+				<PrefectureSelectSm />
+			</ContextProvider>
+		</ThemeProvider>
+	);
+};
+
+// 都道府県チェックボックス(画面サイズsm以下UI時)に関するテスト
+describe('Left Bar Item Component', () => {
+	beforeEach(() => {
+		// モックデータを設定
+		(usePrefecture as jest.Mock).mockReturnValue({
+			prefecrtures: mockPrefectures,
+			selectedPrefectures: mockSelectedPrefectures, // モック配列を参照
+			handleSelectPrefectures: jest.fn(
+				(prefecture: { prefCode: number; prefName: string }) => {
+					mockSelectedPrefectures.push(prefecture);
+				}
+			),
+		});
+
+		(useTheme as jest.Mock).mockReturnValue(darkTheme);
+	});
+
+	// 都道府県チェックボックスの数が都道府県データ47個分かどうか
+	test('renders 47 prefecture items correctly', async () => {
+		render(<TestWrapper />);
+
+		// prefecturesが表示されるまで待機
+		const prefectureElements = screen.getAllByTestId(/checkbox-/);
+
+		// アイテム数が47であることを確認
+		expect(prefectureElements).toHaveLength(47);
+	});
+
+	// 各都道府県の名前が正しく表示されているか
+	test('correctly displays the name of each prefecture', () => {
+		render(<TestWrapper />);
+
+		// モックデータからすべての都道府県名が画面に表示されていることを検証
+		mockPrefectures.forEach((prefecture) => {
+			const elements = screen.getAllByText(prefecture.prefName);
+			expect(elements.length).toBeGreaterThan(0);
+		});
+	});
+
+	// 選択された都道府県stateの初期値は空配列であるか
+	test('should initialize selectedPrefectures state as an empty array', async () => {
+		render(<TestWrapper />);
+
+		// renderHookを用いてusePrefecture hooksから値を取得、テスト
+		const { result } = renderHook(() => usePrefecture());
+
+		// 空配列であるか検証
+		expect(result.current.selectedPrefectures).toEqual([]);
+	});
+
+	// クリック時に都道府県が選択状態となるか(selectedPrefecturesに格納されるか)
+	test('selects the correct prefecture when clicked', async () => {
+		render(<TestWrapper />);
+
+		// renderHookを用いてusePrefecture hooksから値を取得、テスト
+		const { result } = renderHook(() => usePrefecture());
+
+		// 最初の都道府県ボタンを取得
+		const firstPrefectureButton = screen.getByTestId('checkbox-1'); // 北海道のテストID
+
+		// クリックイベントを発火させて都道府県を選択(ここでは北海道)
+		await act(async () => {
+			fireEvent.click(firstPrefectureButton);
+			await result.current.handleSelectPrefectures({
+				prefCode: 1,
+				prefName: '北海道',
+			});
+		});
+
+		// クリック後selectedPrefecturesに{ prefCode: 1, prefName: '北海道' }が格納されているか確認
+		await waitFor(() => {
+			expect(result.current.selectedPrefectures).toContainEqual({
+				prefCode: 1,
+				prefName: '北海道',
+			});
+		});
+	});
+});
