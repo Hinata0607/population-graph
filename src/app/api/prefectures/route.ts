2d840e3422f25febafad46ed40f06d4ea45fe3c7
Hinata0607 <atahin0607@gmail.com>
2024-09-28 15:54:45 +0900

    fix: github ci build error

--- src/app/api/prefectures/route.ts
+++ src/app/api/prefectures/route.ts
@@ -1,11 +1,12 @@
 import { PrefectureResponseProps } from '@/interfaces';
 import { axiosFetch } from '@/libs';
+import { NextResponse } from 'next/server';
 
 // RESASから都道府県一覧情報を取得するAPI
-export const GET = async () => {
+export const GET = async (): Promise<NextResponse> => {
 	const url = `${process.env.RESAS_ENDPOINT}/api/v1/prefectures`;
 	const response = await axiosFetch.get<PrefectureResponseProps>(url, {
 		'X-API-KEY': process.env.RESAS_API_KEY || '',
 	});
-	return response;
+	return NextResponse.json(response);
 };
